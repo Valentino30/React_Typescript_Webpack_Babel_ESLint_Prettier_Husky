8d50775f40a58cd4e3cf8a04a995257a78922347
Valentino <valentino@secret-source.eu>
2022-01-31 16:58:19 +0100

    Add webpack config

---
+++ webpack/webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  // Defines the entry point for the application to be ./src/index.tsx
+  entry: path.resolve(__dirname, "..", "./src/index.tsx"),
+  resolve: {
+    // Allows you to write import statements without using .tsx, .ts and .js
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  module: {
+    rules: [
+      // Tells webpack to use babel to trnaspile all .ts, .tsx, .js and .jsx files
+      {
+        test: /\.(ts|js)x?$/,
+        // Tells webpack to ignore the /node_modules folder for transpilation
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+    ],
+  },
+  // Tells webpack to output the build file to the ./build folder and name it bundle.js
+  output: {
+    path: path.resolve(__dirname, "..", "./build"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  plugins: [
+    // HtmlWebpackPlugin injects the bundle.js file inside the ./src/index.html and moves the ./src/index.html to the ./build folder
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "..", "./src/index.html"),
+    }),
+  ],
+  stats: "errors-only",
+};
