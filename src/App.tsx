9922d5af06e223dd287f73607ee44f79dea91d58
Valentino <valentino@secret-source.eu>
2022-02-01 15:37:37 +0100

    Add webpack hot module replacement

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,31 @@
 import "./styles.css";
 import logo from "./assets/webpack-logo.svg";
 import image from "./assets/webpack-logo.jpeg";
+import Counter from "./components/Counter";
 
 export default function App() {
   return (
-    <div
-      style={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-between",
-      }}
-    >
-      <img src={logo} alt="webpack-logo" width={50} height={50} />
-      <h1>{`Welcome to Webpack by ${process.env.author}: "${process.env.NODE_ENV}"`}</h1>
-      <img src={image} alt="webpack-image" width={50} height={50} />
+    <div>
+      <header
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <img src={logo} alt="webpack-logo" width={50} height={50} />
+        <h1>{`Welcome to Webpack by ${process.env.author}: "${process.env.NODE_ENV}"`}</h1>
+        <img src={image} alt="webpack-image" width={50} height={50} />
+      </header>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <Counter />
+      </div>
     </div>
   );
 }
---
+++ src/components/Counter/index.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <button
+      style={{
+        height: 40,
+        color: "white",
+        border: "none",
+        borderRadius: 5,
+        backgroundColor: "lightskyblue",
+      }}
+      onClick={handleClick}
+    >{`You have clicked me ${count} time${count !== 1 ? "s" : ""}`}</button>
+  );
+}
