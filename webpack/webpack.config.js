fcdec35f66317d4730821c94c0e9f51c68679ea7
Valentino <valentino@secret-source.eu>
2022-02-15 17:12:29 +0100

    Format webpack config files

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,11 +1,11 @@
-const { merge } = require("webpack-merge");
-const commonConfig = require("./webpack.common.js");
+const { merge } = require('webpack-merge')
+const commonConfig = require('./webpack.common.js')
 
 // Merged the common configuration with the dev or prod one depending on the specified environment
 // The envVars are automatically picked up from the scripts "start" or "build"
 module.exports = (envVars) => {
-  const { env } = envVars;
-  const environmentConfig = require(`./webpack.${env}.js`);
-  const config = merge(commonConfig, environmentConfig);
-  return config;
-};
+  const { env } = envVars
+  const environmentConfig = require(`./webpack.${env}.js`)
+  const config = merge(commonConfig, environmentConfig)
+  return config
+}
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,27 +1,27 @@
-const webpack = require("webpack");
-const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+const webpack = require('webpack')
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 // Webpack configuration for the dev environment
 module.exports = {
-  mode: "development",
+  mode: 'development',
   devServer: {
     // This enables webpack's hot module replacement, which prevents the browser from refreshing every time we change something in the code
     hot: true,
     // This substiture the --open flag on the "start" script to automatically launch the browser on start
     open: true,
-     // Added this to avoid Cannot GET on client-side routes other than '/'
+    // Added this to avoid Cannot GET on client-side routes other than '/'
     // Source: https://ui.dev/react-router-cannot-get-url-refresh/
     historyApiFallback: true,
   },
   // This is the devtool used by CRA, many more devtools are available
   // Source-Map: A source-map is a .map file that maps the bundled code to your source code
   // Use: When your budled code errors out, the browser will console log the error pointing at the source code
-  devtool: "cheap-module-source-map",
+  devtool: 'cheap-module-source-map',
   plugins: [
     // This allows you to set any custom environment variable you may need in dev
     new webpack.DefinePlugin({
-      "process.env.author": JSON.stringify("Valentino30"),
+      'process.env.author': JSON.stringify('Valentino30'),
     }),
     new ReactRefreshWebpackPlugin(),
   ],
-};
+}
